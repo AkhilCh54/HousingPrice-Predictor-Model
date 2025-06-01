92736e7d59cbf4d98f9f347d2f86e606a832e68d
AkhilCh54 <akhilcherukuri54@gmail.com>
2025-06-01 16:38:39 -0500

    Remove file from repository

--- testing.py
+++
@@ -1,7 +0,0 @@
-import seaborn as sns
-import pandas as pd
-
-filepath = "./DataAnalysis_and_predictiveModelling_using_amesHousingDataSet/AmesHousing.csv"
-data = pd.read_csv(filepath)
-
-sns.heatmap(data=data)
